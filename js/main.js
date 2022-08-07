604ae85beebd37d2d1f0bc56e4f7657f3e1557a9
somearthzaza <58814744+somearthzaza@users.noreply.github.com>
2022-08-07 23:40:22 +0700

    test moblie

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-document.red
+
+addEventListener("resize" , () =>{
+    const imgfront = document.querySelector(".header-banner-img-front")
+    const imgback = document.querySelector(".header-banner-img-back")
+
+    console.log(window.innerWidth)
+    console.log("frontimg= " + imgfront.clientWidth)
+    console.log(`backimg = ${imgback.clientWidth}`)
+});
